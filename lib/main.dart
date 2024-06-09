309184ecd16201fd6c26ca30a6944485286398d3
sergioIbgz <sergioibagnez@gmail.com>
2024-06-09 12:40:31 -0500

    firebase implementado y pod file actualizado

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,20 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:recetas/config/router/app_router.dart';
 import 'package:recetas/config/theme/app_theme.dart';
 
-void main() => runApp(
-      const ProviderScope(
-        child: MyApp(),
-      ),
-    );
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
